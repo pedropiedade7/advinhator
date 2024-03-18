9439895b3c75083c80bf3d7b1908ef3a2859726a
pedropiedade7 <142631687+pedropiedade7@users.noreply.github.com>
2024-03-18 11:06:46 -0300

    feat: advinhator

---
+++ index.py
@@ -0,0 +1,20 @@
+import time
+
+print("-----------------------------")
+print("SEJA BEM VINDO AO ADVINHATOR")
+print("-----------------------------")
+
+thinked = input("Pense em algo: ")
+
+def advinhator():
+    print("EU IREI ADVINHAR OQ VOCÊ PENSOU")
+    time.sleep(2)
+    print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+    time.sleep(2)
+    print("EU ESTOU VENDO...")
+    time.sleep(2)
+    print("O QUE VOCÊ PENSOU FOI!!!!!!!!!!!!!!!")
+    time.sleep(3)
+    print(thinked)
+
+advinhator()
